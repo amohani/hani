bf7e7b0f19ac49be8055be6ae1d7aecb541a39fc
Hani Khalil <95hani@gmail.com>
2019-10-10 15:31:38 +0200

    hani

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const form = document.querySelector('#form');
 const content = document.querySelector('#content');
-
+window.alert("Observera, endast 20+")
 const age = prompt('Hur gammal är du?');
 
 // check for age of user ..
-if (age > 19) {
+if (age >= 20) {
     // old enough, show website
+    window.alert("Välkommen Max!")
     content.style.display = 'block';
-    document.querySelector('.message').innerHTML ="Välkommen!";
     document.querySelector('#enter').className = 'granted';
-
 } else {
     // not old enough, do not show website
+    window.alert("Sorry, du är inte tillräckligt gammal för att se webbsidan!")
+    window.location.replace("http://google.com");
 content.style.display = 'none';
-document.querySelector('.message').innerHTML = "Sorry, du är inte tillräckligt gammal!";
 document.querySelector('#enter').className = 'denied';
 }
